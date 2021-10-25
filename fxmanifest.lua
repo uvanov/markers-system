65706cdc3bba2d77afc295415bc932fb27b6ab36
uvanov <kirill.ivanov000@gmail.com>
2021-10-25 19:31:43 +0300

    Add Files

---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'bodacious'
+game 'gta5'
+author 'gideon_'
+description 'TypeScript Resourse Example'
+version '1.0.0'
+
+client_script 'dist/client/*.client.js'
+server_script 'dist/server/*.server.js'
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const buildPath = path.resolve(__dirname, 'dist');
+
+const client = {
+    entry: './src/client/client.ts',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                include: path.resolve(__dirname, 'src')
+            }
+        ]
+    },
+
+    watchOptions: {
+        ignored: /node_modules/
+    },
+
+    output: {
+        filename: '[name].client.js',
+        path: path.resolve(buildPath, 'client'),
+        clean: true
+    }
+}
+
+const server = {
+    entry: './src/server/server.ts',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                include: path.resolve(__dirname, 'src')
+            }
+        ]
+    },
+
+    watchOptions: {
+        ignored: /node_modules/
+    },
+
+    output: {
+        filename: '[name].server.js',
+        path: path.resolve(buildPath, 'server'),
+        clean: true
+    }
+}
+
+module.exports = [client, server];
